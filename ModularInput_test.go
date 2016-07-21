a337a01123112026352854aff60756782b419731
Andy Nortrup <andrew.nortrup@gmail.com>
2016-07-20 21:03:29 -0700

    Added a test for reading mod input configs.

--- ModularInput_test.go
+++ ModularInput_test.go
@@ -3,6 +3,7 @@ package splunk
 import (
 	"bytes"
 	"encoding/xml"
+	"strings"
 	"testing"
 )
 
@@ -63,6 +64,40 @@ func modInputConfigCases(result *ModInputConfig, t *testing.T) {
 	if result.Stanzas[0].Params[0].Name != "param1" {
 		t.Logf("Incorrect Stanza Name Returned.  Expecting: param1 Got: %v",
 			result.Stanzas[0].Params[0].Name)
+		t.Fail()
+	}
+}
+
+func TestReadConfig(t *testing.T) {
+	config := `
+	<input>
+	  <server_host>myHost</server_host>
+	  <server_uri>https://127.0.0.1:8089</server_uri>
+	  <session_key>123102983109283019283</session_key>
+	  <checkpoint_dir>/opt/splunk/var/lib/splunk/modinputs</checkpoint_dir>
+	  <configuration>
+	    <stanza name="myScheme://aaa">
+	        <param name="param1">value1</param>
+	        <param name="param2">value2</param>
+	        <param name="disabled">0</param>
+	        <param name="index">default</param>
+	    </stanza>
+	  </configuration>
+	</input>`
+
+	result, err := ReadModInputConfig(strings.NewReader(config))
+	if err != nil {
+		t.Logf("Unable to read ModInputConfig: %v\n", err)
+		t.Fail()
+	}
+
+	if result.SessionKey != "123102983109283019283" {
+		t.Logf("Incorrect Session Key returned. Expected: 123102983109283019283\t Received: %v\n", result.SessionKey)
+		t.Fail()
+	}
+
+	if result.Stanzas[0].StanzaName != "myScheme://aaa" {
+		t.Logf("Incorrect Stanza Name returned. Expected: myScheme://aaa\t Received: %v\n", result.Stanzas[0].StanzaName)
 		t.Fail()
 	}
 }
